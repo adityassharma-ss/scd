5644d06285ffa7797adb634a41d3ad54e06ddd12
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2025-01-07 17:13:56 +0530

    Update New.ps1

--- New.ps1
+++ New.ps1
@@ -1,1 +1,21 @@
+New-SCOMOverride -Monitor (Get-SCOMMonitor -DisplayName "URLGenie Error Code Monitor") `
+    -Parameter "StatusThreshold" `
+    -Value 400 `
+    -ManagementPack (Get-SCOMManagementPack -DisplayName "Overrides Management Pack") `
+    -Target (Get-SCOMClassInstance -Name "YourTargetObjectName")
 
+
+    New-SCOMOverride -Monitor (Get-SCOMMonitor -DisplayName "URLGenie Error Code Monitor") `
+    -Parameter "Enabled" `
+    -Value $false `
+    -ManagementPack (Get-SCOMManagementPack -DisplayName "Overrides Management Pack") `
+    -Target (Get-SCOMClassInstance -Name "YourTargetObjectName")
+
+
+    Set-SCOMClassInstance -Instance (Get-SCOMClassInstance -Name "YourTargetObjectName") -RecalculateHealth
+
+
+
+    Invoke-Command -ComputerName "TargetServerName" -ScriptBlock {
+    Restart-Service HealthService
+}
