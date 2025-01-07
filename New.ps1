955d9837c77b61b8c79e62595fc38f54fe14226f
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2025-01-07 18:21:47 +0530

    Update New.ps1

--- New.ps1
+++ New.ps1
@@ -1,1 +1,10 @@
-Get-SCOMMonitor | Where-Object {$_.DisplayName -match "URL|Web|HTTP"} | Select-Object DisplayName
+Get-SCOMMonitor | Where-Object {$_.DisplayName -match "URL|Web|HTTP"} | ForEach-Object {
+    $monitor = $_
+    $config = Get-SCOMMonitorConfiguration -Monitor $monitor
+    [PSCustomObject]@{
+        DisplayName       = $monitor.DisplayName
+        Target            = $monitor.Target.DisplayName
+        IntervalSeconds   = $config | Where-Object { $_.Property -eq "IntervalSeconds" } | Select-Object -ExpandProperty Value
+        ManagementPack    = $monitor.ManagementPack.DisplayName
+    }
+}
