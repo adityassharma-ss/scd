72cadf62579e15f554830b891020fa4e80b9828a
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2025-02-06 18:53:23 +0530

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,7 +1,15 @@
-SELECT TOP 10 [DateTime], [SampleValue], [CounterName], [Path]  
-FROM [SCOMConfiguration].[dbo].[grafanaPerfReport02]  
-WHERE [ManagementPackSystemName] = 'Microsoft.SystemCenter.2007'  
-AND [ObjectName] = 'System'  
-AND [CounterName] = 'System Up Time'  
-AND [Path] IN ($ServerName)  
-ORDER BY [DateTime] DESC;
+SELECT
+    $_timeEpoch([DateTime]),
+    CASE 
+        WHEN [SampleValue] > 0 THEN 1  -- Up
+        ELSE 0  -- Down
+    END as status
+FROM
+    [SCOMConfiguration].[dbo].[grafana PerfReport02]
+WHERE
+    [ManagementPackSystemName] = 'Microsoft.SystemCenter.2007'
+    AND [ObjectName] = 'System'
+    AND [CounterName] = 'System Up Time'
+    AND [Path] IN ($ServerName)
+ORDER BY
+    [DateTime] ASC
