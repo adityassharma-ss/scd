6219b2c105cb8dbfdb035270f5777368aa978de1
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2025-02-06 18:46:06 +0530

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,16 +1,7 @@
-SELECT 
-    CONVERT(BIGINT, DATEDIFF(SECOND, '1970-01-01', [DateTime])) AS time, 
-    CASE 
-        WHEN [SampleValue] > 0 THEN 'Up' 
-        ELSE 'Down' 
-    END AS value, 
-    [CounterName] AS metric
-FROM 
-    [SCOMConfiguration].[dbo].[grafanaPerfReport02]
-WHERE 
-    [ManagementPackSystemName] = 'Microsoft.SystemCenter.2007'
-    AND [ObjectName] = 'System'
-    AND [CounterName] = 'System Up Time'
-    AND [Path] IN ($ServerName)
-ORDER BY 
-    [DateTime] ASC;
+SELECT TOP 10 [DateTime], [SampleValue], [CounterName], [Path]  
+FROM [SCOMConfiguration].[dbo].[grafanaPerfReport02]  
+WHERE [ManagementPackSystemName] = 'Microsoft.SystemCenter.2007'  
+AND [ObjectName] = 'System'  
+AND [CounterName] = 'System Up Time'  
+AND [Path] IN ($ServerName)  
+ORDER BY [DateTime] DESC;
